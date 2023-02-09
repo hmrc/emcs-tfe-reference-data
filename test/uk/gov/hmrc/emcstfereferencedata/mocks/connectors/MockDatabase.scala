3804cd8828cae1da000fdf61927e8f6307c6733a
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2023-02-09 09:19:29 +0000

    [ETFE-297-Matt] Refactor the MockDatabase class

--- test/uk/gov/hmrc/emcstfereferencedata/mocks/connectors/MockDatabase.scala
+++ test/uk/gov/hmrc/emcstfereferencedata/mocks/connectors/MockDatabase.scala
@@ -6,88 +6,50 @@
 package uk.gov.hmrc.emcstfereferencedata.mocks.connectors
 
 
-import org.scalamock.handlers.{CallHandler0, CallHandler1, CallHandler2}
 import org.scalamock.scalatest.MockFactory
 import play.api.db.Database
 
-import org.scalatest.matchers._
-import uk.gov.hmrc.http.HeaderCarrier
-
 import java.sql.{Connection, ResultSet, ResultSetMetaData, Statement}
-import scala.Option.when
-import scala.concurrent.{ExecutionContext, Future}
 
 trait MockDatabase extends MockFactory {
 
   val mockDatabase: Database = mock[Database]
 
   object MockedDatabase extends MockFactory {
-    def mockADatabaseAndResultSet(columnNames: Seq[String], rowValues:Option[Seq[String]]) = {
+    def mockADatabaseAndResultSet(columnNames: Seq[String], rowValues:Option[Seq[String]]): Unit = {
       val mockConnection = mock[Connection]
       val mockStatement = mock[Statement]
       val mockResultSet = mock[ResultSet]
       val mockResultSetMetaData = mock[ResultSetMetaData]
 
-        (() => mockResultSet.getMetaData()).expects().returns(mockResultSetMetaData)
+      (() => mockResultSet.getMetaData).expects().returns(mockResultSetMetaData).anyNumberOfTimes()
+      (() => mockResultSetMetaData.getColumnCount).expects().returns(columnNames.size).anyNumberOfTimes()
 
+      (1 to columnNames.size).foreach(x => (mockResultSetMetaData.getColumnName(_: Int)).expects(x).returns(columnNames(x-1)).noMoreThanOnce())
 
+      rowValues match {
+        case Some(rowLevelValues) =>
+          (1 to columnNames.size).foreach(x => (mockResultSet.getString(_: Int)).expects(x).returns(rowLevelValues(x-1)).noMoreThanOnce())
 
-        (() =>mockResultSetMetaData.getColumnCount).expects().returns(columnNames.size)
+          (() => mockResultSet.isBeforeFirst()).expects().returns(true).noMoreThanOnce()
+          (() => mockResultSet.isBeforeFirst()).expects().returns(false).anyNumberOfTimes()
+          (() => mockResultSet.next()).expects().returns(true).noMoreThanOnce()
+          (() => mockResultSet.next()).expects().returns(false).anyNumberOfTimes()
+          (() => mockResultSet.getRow()).expects().returns(1).anyNumberOfTimes()
 
-      def getColumnName(indexPosition: Int): CallHandler1[Int, String] = {
-        (mockResultSetMetaData.getColumnClassName(_: Int)).expects(indexPosition).returns(columnNames(indexPosition - 1))
+        case None =>
+          (() => mockResultSet.isBeforeFirst()).expects.returns(false).anyNumberOfTimes()
+          (() => mockResultSet.next()).expects.returns(false).anyNumberOfTimes()
       }
 
 
-        (1 to columnNames.size).foreach(
-          x => getColumnName(x)
-        )
+      (mockDatabase.getConnection(_: Boolean)).expects(*).returns(mockConnection).anyNumberOfTimes()
+      (() => mockConnection.createStatement()).expects().returns(mockStatement).anyNumberOfTimes()
+      (mockStatement.executeQuery(_: String)).expects(*).returns(mockResultSet).anyNumberOfTimes()
 
-
-      def getColumnValues(indexPosition: Int, rowLevelValues: Seq[String]): CallHandler1[Int, String] = {
-        (mockResultSet.getString(_: Int)).expects(indexPosition).returns(rowLevelValues(indexPosition - 1))
-      }
-
-      def getRowValues(columnSize: Int, rowLevelValues: Seq[String]): Unit = {
-        (1 to columnSize).foreach(
-          x => getColumnValues(x, rowLevelValues)
-        )
-      }
-
-      println(Console.MAGENTA + rowValues + Console.RESET)
-
-        rowValues match {
-          case Some(rowLevelValues) =>
-            getRowValues(columnNames.size, rowLevelValues)
-            (() => mockResultSet.isBeforeFirst).expects().returns(true).returns(false)
-            (() => mockResultSet.next).expects().returns(true).returns(false)
-            (() => mockResultSet.getRow).expects().returns(1)
-          case None =>
-            (() => mockResultSet.isBeforeFirst).expects().returns(false)
-            (() => mockResultSet.next()).expects().returns(true)
-
-
-        }
-
-
-
-
-        (mockDatabase.getConnection(_: Boolean)).expects(*).returns(mockConnection)
-
-
-
-        (() => mockConnection.createStatement()).expects().returns(mockStatement)
-
-
-
-        (mockStatement.executeQuery(_: String)).expects(*).returns(mockResultSet)
-
-
-
-
-//      doNothing().when(mockConnection).rollback()
-//      doNothing().when(mockConnection).commit()
-//      doNothing().when(mockConnection).close()
+      (() => mockConnection.rollback()).expects().returns({}).anyNumberOfTimes()
+      (() => mockConnection.commit()).expects().returns({}).anyNumberOfTimes()
+      (() => mockConnection.close()).expects().returns({}).anyNumberOfTimes()
     }
   }
 }
