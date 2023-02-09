541b050e76c491006827bd6ca1d835880a3c0e1b
dwalters <32767569+david-walters-HMRC@users.noreply.github.com>
2023-02-09 16:29:57 +0000

    ETFE-297 fixed rebase issues

--- it/uk/gov/hmrc/emcstfereferencedata/OracleDBControllerIntergrationSpec.scala
+++ it/uk/gov/hmrc/emcstfereferencedata/OracleDBControllerIntergrationSpec.scala
@@ -1,6 +1,17 @@
 /*
  * Copyright 2023 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.emcstfereferencedata
@@ -9,7 +20,6 @@ import com.github.tomakehurst.wiremock.stubbing.StubMapping
 import play.api.http.Status
 import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.{WSRequest, WSResponse}
-import uk.gov.hmrc.emcstfereferencedata.stubs.ReferenceDataStub
 import uk.gov.hmrc.emcstfereferencedata.support.IntegrationBaseSpec
 
 import scala.xml.Elem
